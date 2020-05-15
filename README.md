6daf1d7e46b185263a588feb3f109fc9afbd2a97
Thanatos <thanatos-dev@outlook.com>
2020-05-15 21:01:08 +0200

    autoLearn

---
+++ README.md
@@ -0,0 +1,32 @@
+# ![logo](https://raw.githubusercontent.com/azerothcore/azerothcore.github.io/master/images/logo-github.png) AzerothCore
+
+# autoLearn
+
+### Description
+
+		I share my autoLearn System.
+
+		autoLearn is a script that allows players to learns spells on Levelup.
+
+    Repository: https://github.com/Open-Wow/lua-autolearn
+    Download: https://github.com/Open-Wow/lua-autolearn/archive/master.zip
+    License: AGPL
+
+
+# Usage
+
+- Download the [release](https://github.com/Open-Wow/lua-autolearn/archive/master.zip).
+- Just **drop the lua-autolearn.lua on your *lua_scripts* folder.**
+- **Restart** your server.
+- Enjoy :D
+
+# Need help?
+
+If you encounter a bug please [open a issue](https://github.com/Open-Wow/lua-autolearn/issues).
+
+
+## Credits
+
+* [open-wow](https://github.com/Open-Wow)
+
+AzerothCore: [repository](https://github.com/azerothcore) - [website](http://azerothcore.org/) - [discord chat community](https://discord.gg/PaqQRkd)
---
+++ lua-autolearn.lua
@@ -0,0 +1,71 @@
+--[[
+
+                         __                __
+                        /  |              /  |
+    ______   __    __  _$$ |_     ______  $$ |        ______    ______    ______   _______
+   /      \ /  |  /  |/ $$   |   /      \ $$ |       /      \  /      \  /      \ /       \
+   $$$$$$  |$$ |  $$ |$$$$$$/   /$$$$$$  |$$ |      /$$$$$$  | $$$$$$  |/$$$$$$  |$$$$$$$  |
+   /    $$ |$$ |  $$ |  $$ | __ $$ |  $$ |$$ |      $$    $$ | /    $$ |$$ |  $$/ $$ |  $$ |
+  /$$$$$$$ |$$ \__$$ |  $$ |/  |$$ \__$$ |$$ |_____ $$$$$$$$/ /$$$$$$$ |$$ |      $$ |  $$ |
+  $$    $$ |$$    $$/   $$  $$/ $$    $$/ $$       |$$       |$$    $$ |$$ |      $$ |  $$ |
+   $$$$$$$/  $$$$$$/     $$$$/   $$$$$$/  $$$$$$$$/  $$$$$$$/  $$$$$$$/ $$/       $$/   $$/
+
+
+  @Original_Author : Open-Wow
+
+  This script allow player to learn spells on levelUp.
+
+  @Open-Wow Github : https://github.com/Open-Wow
+  @Open-Wow Site : https://open-wow.fr
+
+  @AzerothCore Community : https://discord.gg/PaqQRkd
+  @Open-Wow Community : https://discord.gg/mTHxKmY
+
+  Thanks for your patience <3.
+
+]]--
+
+local autoLearn = {};
+
+autoLearn.Spell = {}
+
+autoLearn.Class = {
+  [1] = {200001, 200002},
+  [2] = {200003, 200004},
+  [3] = {200013, 200014},
+  [4] = {200015, 200016},
+  [5] = {200011, 200012},
+  [6] = {200019},
+  [7] = {200017, 200018},
+  [8] = {200007, 200008},
+  [9] = {200009, 200010},
+  [11] = {200005, 200006};
+};
+
+
+function autoLearn.onServerStart(event)
+  local getSpell = WorldDBQuery('SELECT ID, SpellID, ReqLevel FROM `npc_trainer` WHERE `ID` > 200000 AND `ID` < 200020 AND `ReqSkillLine` = 0 ORDER BY `ID` ASC;');
+  repeat
+    table.insert(autoLearn.Spell, {getSpell:GetUInt32(0), getSpell:GetUInt32(1), getSpell:GetUInt32(2)});
+  until not getSpell:NextRow();
+end
+RegisterServerEvent(14, autoLearn.onServerStart);
+
+function autoLearn.onLevelUp(event, player, oldLevel)
+  local pClass = player:GetClass();
+
+  for _, class in pairs(autoLearn.Class[pClass])do
+
+    for _, value in pairs(autoLearn.Spell) do
+
+      if (value[1] == class)then
+
+        if ((oldLevel +1 ) >= value[3]) then
+
+          player:LearnSpell(value[2]);
+        end
+      end
+    end
+  end
+end
+RegisterPlayerEvent(13, autoLearn.onLevelUp);
